464148acb7d9e5384b9c37280d2ab7bf3e829949
valeriy <valera-alekseev-2007@inbox.ru>
2024-05-11 12:57:59 +0300

    work run

---
+++ src/module/client/dialog_manager.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+from src.module.client.widgets.custommddialog import CustomMDDialog
+
+
+@dataclass
+class DialogManager:
+	dialogs = {}
+
+	def __call__(self, name: str = None, item=None) -> None:
+		match bool(name):
+			case True:
+				match self.dialogs[name].is_open():
+					case True:
+						self.dialogs[name].dismiss()
+					case False:
+						self.dialogs[name].open()
+			case False:
+				def recall(dialog: CustomMDDialog) -> None:
+					if dialog.is_open():
+						dialog.dismiss()
+				tuple(map(recall, self.dialogs.values()))
+
+	def __setitem__(self, key, value):
+		try:
+			assert type(key) is str, 'TypeError'
+			assert type(value) is CustomMDDialog, 'TypeError'
+			assert key not in self.dialogs, 'KeyError'
+			self.dialogs[key] = value
+		except AssertionError as error:
+			match str(error):
+				case "TypeError":
+					raise TypeError
+				case "KeyError":
+					raise KeyError
+
+	def __getitem__(self, item: str) -> CustomMDDialog:
+		try:
+			assert item in self.dialogs
+			return self.dialogs[item]
+		except AssertionError:
+			raise KeyError
+
+	def __str__(self):
+		return str(self.dialogs)
+
+
+if __name__ == '__main__':
+	a = DialogManager()
+	print(a)
